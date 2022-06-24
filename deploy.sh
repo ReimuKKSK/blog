6b5849d4030e71fe5db8d38d933d189adac4a5de
Souiken <souiken@coffeeflavoredtea.team>
2022-06-25 01:48:57 +0800

    反正也没人看

--- deploy.sh
+++ deploy.sh
@@ -10,16 +10,16 @@ npm run build
 cd docs/.vuepress/dist
 
 # 如果是发布到自定义域名
-# echo 'souiken.top' > CNAME
+echo 'souiken.top' > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
 
 # 如果你想要部署到 https://USERNAME.github.io
-git push -f git@github.com:Souiken/souiken.github.io.git master
+git push -f git@github.com:Souiken/souiken.github.io.git main
 
 # 如果发布到 https://USERNAME.github.io/<REPO>  REPO=github上的项目
-# git push -f git@github.com:USERNAME/<REPO>.git master:gh-pages
+# git push -f git@github.com:Souiken/souiken.github.io main:gh-pages
 
 cd -
--- docs/README.md
+++ docs/README.md
@@ -4,7 +4,7 @@ heroImage: https://s1.328888.xyz/2022/06/04/W5vYg.jpg
 heroText: 418
 tagline: I'm a teapot.
 actionText: 快速摆烂 →
-actionLink: /test/
+actionLink: /main/
 features:
 - title: 摆烂
   details: 这个博客专为摆烂而生,里面的内容全是摆烂。
@@ -12,5 +12,5 @@ features:
   details: 由 Github 支持的 Github Page 所驱动,仅支持静态页面!
 - title: 高性能
   details: 使用了静态页面,所有计算都会交给访问端!
-footer: MIT Licensed | Copyright © 2022 Souiken
+footer: Baka Licensed | Copyright © 2022 Souiken
 ---
